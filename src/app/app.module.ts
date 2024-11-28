ac24687e6d5fc656749de3a3f1422500676f0b56
Milos Marcetic <marcetic.judo@gmail.com>
2024-11-29 00:46:34 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 import { AppComponent } from './app.component';
+import { MapboxModule } from 'ngx-mapbox-gl';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgxMapboxGLModule
+    NgxMapboxGLModule,
+    MapboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
